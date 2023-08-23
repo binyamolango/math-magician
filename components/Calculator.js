655606ec74b95c3262aa119d7609348822d3a2ad
binyamolango <olangobinyam@gmail.com>
2023-08-23 05:46:16 +0300

    Edit components in calculator.js file

--- components/Calculator.js
+++ components/Calculator.js
@@ -1,85 +1,45 @@
 const Body = (props) => {
   return (
-    <div class="container">
-      <div class="top-layer">
-        <p class="top-value">0</p>
+    <div className="container grid">
+
+      <div className="row1">
+        <div className="top-layer">0</div>
       </div>
 
-      <button id="side-layer1" class="side-layer rect">
-        <p class="side-value">/</p>
-      </button>
+      <div className="row2 rowSame">
+        <button id="main-layer1" className="main-layer rect" label="AC" />
+        <button id="main-layer2" className="main-layer rect" label="+/-" />
+        <button id="main-layer3" className="main-layer rect" label="%" />
+        <button id="side-layer1" className="side-layer rect" label="/" />
+      </div>
 
-      <button id="side-layer2" class="side-layer rect">
-        <p class="side-value">*</p>
-      </button>
+      <div className="row3 rowSame"> 
+        <button id="main-layer4" className="main-layer rect" label="7" />
+        <button id="main-layer5" className="main-layer rect" label="8" />
+        <button id="main-layer6" className="main-layer rect" label="9" />
+        <button id="side-layer2" className="side-layer rect" label="*" />
+      </div>
 
-      <button id="side-layer3" class="side-layer rect">
-        <p class="side-value">-</p>
-      </button>
+      <div className="row4 rowSame">
+        <button id="main-layer7" className="main-layer rect" label="4" />
+        <button id="main-layer8" className="main-layer rect" label="5" />
+        <button id="main-layer9" className="main-layer rect" label="6" />
+        <button id="side-layer3" className="side-layer rect" label="-" />
+      </div>
 
-      <button id="side-layer4" class="side-layer rect">
-        <p class="side-value">+</p>
-      </button>
+      <div className="row5 rowSame">
+        <button id="main-layer10" className="main-layer rect" label="1" />
+        <button id="main-layer11" className="main-layer rect" label="2" />
+        <button id="main-layer12" className="main-layer rect" label="3" />
+        <button id="side-layer4" className="side-layer rect" label="+" />
+      </div>
 
-      <button id="side-layer5" class="side-layer rect">
-        <p class="side-value">=</p>
-      </button>
+      <div className="row6">
+        <button className="large-rect" label="0" />
+        <button id="main-layer13" className="main-layer rect" label="." />
+        <button id="side-layer5" className="side-layer rect" label="=" />
+      </div>
 
-      <button id="main-layer1" class="main-layer rect">
-        <p>AC</p>
-      </button>
-      
-      <button id="main-layer2" class="main-layer rect">
-        <p>+/-</p>
-      </button>
-      
-      <button id="main-layer3" class="main-layer rect">
-        <p>%</p>
-      </button>
-      
-      <button id="main-layer4" class="main-layer rect">
-        <p>7</p>
-      </button>
-      
-      <button id="main-layer5" class="main-layer rect">
-        <p>8</p>
-      </button>
-      
-      <button id="main-layer6" class="main-layer rect">
-        <p>9</p>
-      </button>
-      
-      <button id="main-layer7" class="main-layer rect">
-        <p>4</p>
-      </button>
-      
-      <button id="main-layer8" class="main-layer rect">
-        <p>5</p>
-      </button>
-      
-      <button id="main-layer9" class="main-layer rect">
-        <p>6</p>
-      </button>
-      
-      <button id="main-layer10" class="main-layer rect">
-        <p>1</p>
-      </button>
-      
-      <button id="main-layer11" class="main-layer rect">
-        <p>2</p>
-      </button>
-      
-      <button id="main-layer12" class="main-layer rect">
-        <p>3</p>
-      </button>
-      
-      <button id="main-layer13" class="main-layer rect">
-        <p>.</p>
-      </button>
-      
-      <button class="large-rect">
-        <p class="bottom-value">0</p>
-      </button>
     </div>
   );
 }

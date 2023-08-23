e9fc19e701d4a0bbdfb42d2eb8688ce4fff5e470
binyamolango <olangobinyam@gmail.com>
2023-08-23 07:00:44 +0300

    Add grid in the style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
+import React from 'react';
+import Calculator from './components/Calculator';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <Calculator />
+  </div>
+);
 
 export default App;
---
+++ src/components/Calculator.js
@@ -0,0 +1,45 @@
+const Calculator = () => (
+  <div className="container grid">
+
+    <div className="row1">
+      <div className="top-layer">0</div>
+    </div>
+
+    <div className="row2 rowSame">
+      <button type="button" id="main-layer1" className="main-layer rect" label="AC" />
+      <button type="button" id="main-layer2" className="main-layer rect" label="+/-" />
+      <button type="button" id="main-layer3" className="main-layer rect" label="%" />
+      <button type="button" id="side-layer1" className="side-layer rect" label="/" />
+    </div>
+
+    <div className="row3 rowSame">
+      <button type="button" id="main-layer4" className="main-layer rect" label="7" />
+      <button type="button" id="main-layer5" className="main-layer rect" label="8" />
+      <button type="button" id="main-layer6" className="main-layer rect" label="9" />
+      <button type="button" id="side-layer2" className="side-layer rect" label="*" />
+    </div>
+
+    <div className="row4 rowSame">
+      <button type="button" id="main-layer7" className="main-layer rect" label="4" />
+      <button type="button" id="main-layer8" className="main-layer rect" label="5" />
+      <button type="button" id="main-layer9" className="main-layer rect" label="6" />
+      <button type="button" id="side-layer3" className="side-layer rect" label="-" />
+    </div>
+
+    <div className="row5 rowSame">
+      <button type="button" id="main-layer10" className="main-layer rect" label="1" />
+      <button type="button" id="main-layer11" className="main-layer rect" label="2" />
+      <button type="button" id="main-layer12" className="main-layer rect" label="3" />
+      <button type="button" id="side-layer4" className="side-layer rect" label="+" />
+    </div>
+
+    <div className="row6 rowSame">
+      <button type="button" className="large-rect" label="0" />
+      <button type="button" id="main-layer13" className="main-layer small-rect" label="." />
+      <button type="button" id="side-layer5" className="side-layer small-rect" label="=" />
+    </div>
+
+  </div>
+);
+
+export default Calculator;

2e59e3cab011b4d06f6c81117d32ee333abe67ff
binyamolango <olangobinyam@gmail.com>
2023-08-28 12:51:55 +0300

    Update quote component

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -3,54 +3,38 @@ import { useState, useEffect } from 'react';
 const Quotes = () => {
   const [quote, setQuote] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('');
 
   useEffect(() => {
-    const apiKey = 'qqoMXsAord0KkBJuAevFSw==E3XSAIz6orWVAcZM';
-    const category = 'health';
+    const category = 'happiness';
+    const API_KEY = 'qqoMXsAord0KkBJuAevFSw==E3XSAIz6orWVAcZM';
+    const baseURL = 'https://api.api-ninjas.com/v1/quotes?category=';
 
-    fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+    fetch(baseURL + category, {
       headers: {
-        'X-Api-Key': apiKey,
+        'X-Api-Key': API_KEY,
       },
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
+      .then((res) => res.json())
       .then((data) => {
         const randomQuote = data[Math.floor(Math.random() * data.length)];
         setQuote(randomQuote);
-        setLoading(false);
-      })
-      .catch((error) => {
-        setError(error.message);
         setLoading(false);
       });
   }, []);
 
   if (loading) {
-    return <div className="loading">Loading...</div>;
-  }
-
-  if (error) {
-    return (
-      <div className="error">
-        Error:
-        {error}
-      </div>
-    );
+    return (<div>loading...</div>);
   }
 
   return (
-    <div className="quote-display">
-      <h2>Random Quotes</h2>
-      <p>{quote.quote}</p>
-      <p>
+    <div className="quote-display container">
+      <h1>Random Quotes</h1>
+      <p className="quote">
+        { quote.quote }
+      </p>
+      <p className="author">
         -
-        {quote.author}
+        { quote.author }
       </p>
     </div>
   );

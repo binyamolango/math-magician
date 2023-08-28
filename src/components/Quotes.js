cefc14d50317c2aaf6ce3ccad42a1ada268ef0f0
binyamolango <olangobinyam@gmail.com>
2023-08-28 13:26:56 +0300

    Update quotes

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 const Quotes = () => {
   const [quote, setQuote] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const category = 'happiness';
@@ -14,11 +15,21 @@ const Quotes = () => {
         'X-Api-Key': API_KEY,
       },
     })
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Could not fetch the data!');
+        }
+        return res.json();
+      })
       .then((data) => {
         const randomQuote = data[Math.floor(Math.random() * data.length)];
         setQuote(randomQuote);
         setLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(err.message);
       });
   }, []);
 
@@ -26,6 +37,7 @@ const Quotes = () => {
     <div className="quote-display container">
       <h1>Random Quotes</h1>
       { loading && <div>loading...</div> }
+      { error && <div>{error}</div> }
       <quote className="quote">
         { quote.quote }
       </quote>

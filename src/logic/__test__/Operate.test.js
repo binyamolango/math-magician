b6902a158b81f6bd30746a379dcf7bb02b137357
binyamolango <olangobinyam@gmail.com>
2023-08-30 15:20:02 +0300

    Add tests for calculate and operate modules

---
+++ src/logic/__test__/Operate.test.js
@@ -0,0 +1,39 @@
+import Operate from '../Operate';
+
+describe('Operate', () => {
+  it('should give the correct addition of two numbers', () => {
+    const numberOne = '7';
+    const numberTwo = '8';
+    const operation = '+';
+
+    const result = Operate(numberOne, numberTwo, operation);
+    expect(result).toEqual('15');
+  });
+
+  it('should give the correct subtraction of two numbers', () => {
+    const numberOne = '7';
+    const numberTwo = '3';
+    const operation = '-';
+
+    const result = Operate(numberOne, numberTwo, operation);
+    expect(result).toEqual('4');
+  });
+
+  it('should give the correct multiplication of two numbers', () => {
+    const numberOne = '2';
+    const numberTwo = '8';
+    const operation = '*';
+
+    const result = Operate(numberOne, numberTwo, operation);
+    expect(result).toEqual('16');
+  });
+
+  it('should give the correct division of two numbers', () => {
+    const numberOne = '10';
+    const numberTwo = '5';
+    const operation = '/';
+
+    const result = Operate(numberOne, numberTwo, operation);
+    expect(result).toEqual('2');
+  });
+});
